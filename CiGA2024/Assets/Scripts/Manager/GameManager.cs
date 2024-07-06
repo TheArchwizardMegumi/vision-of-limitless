a5f2837595199f2d3ce327e02e0a967280953031
Izzy <3332099165@qq.com>
2024-07-06 16:09:20 +0800

    Ensure full run

--- CiGA2024/Assets/Scripts/Manager/GameManager.cs
+++ CiGA2024/Assets/Scripts/Manager/GameManager.cs
@@ -8,16 +8,16 @@ public class GameManager : Singleton<GameManager>
     public LevelContainer levelContainer;
     int currentLevelIndex = 0;
 
-    // void Awake()
-    // {
-    //     base.Awake();
-    // }
+    void Awake()
+    {
+        base.Awake();
+    }
 
     void Start()
     {
         // 需要加在这里，因为场景会销毁，所以需要重新注册
         Messenger.AddListener(MsgType.playerHurt, ReloadLevel);
-        Messenger.AddListener(MsgType.playerWin, LoadNextLevel); 
+        Messenger.AddListener(MsgType.playerWin, LoadNextLevel);
     }
 
     void Update()
@@ -25,10 +25,11 @@ public class GameManager : Singleton<GameManager>
         
     }
 
-    public void LoadLevel()
+    public void LoadLevel(int index)
     {
-        // int index;
-        // currentLevelIndex = index; 
+        SceneManager.LoadSceneAsync(levelContainer.levels[index-1].name);
+        currentLevelIndex = index-1;
+        PlayerPrefs.SetInt("currentLevelIndex", currentLevelIndex);
     }
 
     private void LoadNextLevel()
@@ -38,13 +39,14 @@ public class GameManager : Singleton<GameManager>
             Debug.Log("No more levels");
             return;
         }
+        currentLevelIndex = PlayerPrefs.GetInt("currentLevelIndex", 0);
         currentLevelIndex += 1;
+        PlayerPrefs.SetInt("currentLevelIndex", currentLevelIndex);
         SceneManager.LoadSceneAsync(levelContainer.levels[currentLevelIndex].name);
     }
 
     private void ReloadLevel()
     {
-        Debug.Log(levelContainer.levels[currentLevelIndex]);
         SceneManager.LoadSceneAsync(levelContainer.levels[currentLevelIndex].name);
     }
 
