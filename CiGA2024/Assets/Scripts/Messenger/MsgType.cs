873828166608597af9f53ad2c1d74aebaf97b73b
Izzy <3332099165@qq.com>
2024-07-09 00:49:42 +0800

    Add restart level component

--- CiGA2024/Assets/Scripts/Messenger/MsgType.cs
+++ CiGA2024/Assets/Scripts/Messenger/MsgType.cs
@@ -4,9 +4,6 @@ public static class MsgType
     public static string IntEvent = nameof(IntEvent);
 
     // in-game events
-    public static string ChangeGold = nameof(ChangeGold);
-    public static string ChangeHealth = nameof(ChangeHealth);
-    public static string ChangeWaves = nameof(ChangeWaves);
 
     public static string GameOver = nameof(GameOver);
     public static string GameWin = nameof(GameWin);
